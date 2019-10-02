17ad2890a056efe58c8a98026a7add719a0574ea
Fernando Tso <fernando_tso@yahoo.com.br>
2019-10-01 22:09:30 -0300

    Incluso entidade

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedio ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+
+        public int id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Naome { get; set; }
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Um usuário pode ter nenhum ou muitos pedidos 
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+
+    }
+}
